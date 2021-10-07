b1c149b06aa9d18cd97910a252b76f07c19ab742
Atif Mustaffa <atifmustaffa@gmail.com>
2021-10-07 17:08:49 +0800

    feat: Add page scroller

--- src/pages/Shop.vue
+++ src/pages/Shop.vue
@@ -47,6 +47,14 @@
         <q-btn flat icon="add_shopping_cart">Add to cart</q-btn>
       </q-card-actions>
     </q-card>
+
+    <q-page-scroller
+      position="bottom-right"
+      :scroll-offset="150"
+      :offset="[18, 18]"
+    >
+      <q-btn fab icon="keyboard_arrow_up" color="accent" />
+    </q-page-scroller>
   </q-page>
 </template>
 
