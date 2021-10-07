9fa891bccd7e5f0a9f3033544b97ec27e85f355f
Atif Mustaffa <atifmustaffa@gmail.com>
2021-10-07 13:51:33 +0800

    refactor: Change essentiallink list

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,49 +1,45 @@
 <template>
-  <q-item
-    clickable
-    tag="a"
-    target="_blank"
-    :href="link"
-  >
-    <q-item-section
-      v-if="icon"
-      avatar
-    >
-      <q-icon :name="icon" />
-    </q-item-section>
+  <div>
+    <q-item clickable tag="a" target="_blank" :href="link">
+      <q-item-section v-if="icon" avatar>
+        <q-icon :name="icon" />
+      </q-item-section>
 
-    <q-item-section>
-      <q-item-label>{{ title }}</q-item-label>
-      <q-item-label caption>
-        {{ caption }}
-      </q-item-label>
-    </q-item-section>
-  </q-item>
+      <q-item-section>
+        <q-item-label>{{ title }}</q-item-label>
+        <q-item-label caption>
+          {{ caption }}
+        </q-item-label>
+      </q-item-section>
+    </q-item>
+    <q-separator v-if="separator" />
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'EssentialLink',
+  name: "EssentialLink",
   props: {
     title: {
       type: String,
       required: true
     },
-
     caption: {
       type: String,
-      default: ''
+      default: ""
     },
-
     link: {
       type: String,
-      default: '#'
+      default: "#"
     },
-
     icon: {
       type: String,
-      default: ''
+      default: ""
+    },
+    separator: {
+      type: Boolean,
+      default: false
     }
   }
-}
+};
 </script>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -26,10 +26,7 @@
       content-class="bg-grey-1"
     >
       <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
+        <q-item-label header class="text-grey-8">
           Essential Links
         </q-item-label>
         <EssentialLink
@@ -47,50 +44,28 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
+import { date } from "quasar";
+import EssentialLink from "components/EssentialLink.vue";
 
 const linksData = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: "Shop",
+    caption: "Browse items",
+    icon: "shopping_bag",
+    link: "/"
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: "Cart",
+    caption: "Manage items in cart",
+    icon: "shopping_cart",
+    link: "/",
+    separator: true
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: "Help",
+    caption: "Services and information",
+    icon: "help",
+    link: "/"
   }
 ];
 
