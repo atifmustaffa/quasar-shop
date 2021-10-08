2c7ba158af6155c136e9fdc22024559ba3f353ee
Atif Mustaffa <atifmustaffa@gmail.com>
2021-10-08 18:05:02 +0800

    refactor: Make drawer not auto open on breakpoint

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -19,12 +19,7 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      content-class="bg-grey-1"
-    >
+    <q-drawer v-model="leftDrawerOpen" bordered content-class="bg-grey-1">
       <q-list>
         <q-item-label header class="text-grey-8">
           Essential Links
