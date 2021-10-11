d23ac4d219e107061f1baeb1e58ca0fe21f21293
Atif Mustaffa <atifmustaffa@gmail.com>
2021-10-11 11:20:08 +0800

    feat: Use CartItem component to list items

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -1,11 +1,35 @@
 <template>
-  <div>
-    Cart page
+  <div class="q-pa-md cart-items">
+    <div class="row q-mb-sm text-weight-bold">
+      <div class="col-11">Details</div>
+      <div class="col-1 text-center">Qty</div>
+    </div>
+
+    <!-- CartItem -->
+    <CartItem
+      v-for="item in itemsCount"
+      :key="item.id"
+      v-bind="item"
+    ></CartItem>
   </div>
 </template>
 
 <script>
+import CartItem from "components/CartItem.vue";
+
 export default {
-  name: "PageCart"
+  name: "PageCart",
+  components: { CartItem },
+  computed: {
+    itemsCount() {
+      return this.$store.getters["cartModule/getItems"];
+    }
+  }
 };
 </script>
+<style lang="scss">
+.cart-items {
+  max-width: 600px;
+  margin: auto;
+}
+</style>
