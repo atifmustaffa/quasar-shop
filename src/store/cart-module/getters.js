7f153e33c3e982db8179845b82d2e749e5f1cd4a
Atif Mustaffa <atifmustaffa@gmail.com>
2021-10-11 11:19:15 +0800

    feat: Store item count

--- src/store/cart-module/getters.js
+++ src/store/cart-module/getters.js
@@ -1,3 +1,6 @@
 export function getLength(state) {
   return state.cart.length;
 }
+export function getItems(state) {
+  return state.cart;
+}
--- src/store/cart-module/mutations.js
+++ src/store/cart-module/mutations.js
@@ -1,5 +1,11 @@
 export function updateCart(state, id) {
-  state.cart.push(id);
+  // Add or increment item count
+  let existingId = state.cart.findIndex(item => item.id === id);
+  if (existingId < 0) {
+    state.cart.push({ id, count: 1 });
+  } else {
+    state.cart[existingId].count++;
+  }
 }
 export function resetCart(state) {
   state.cart = [];
